8cc97413e349d2cb1db6e602fa79190493527ffb
youssefalaa10 <youssefalaacj@gmail.com>
2023-04-30 09:21:29 +0300

    create book model

---
+++ lib/Features/home/data/models/book_model/rental_duration.dart
@@ -0,0 +1,23 @@
+import 'package:equatable/equatable.dart';
+
+class RentalDuration extends Equatable {
+  final String? unit;
+  final int? count;
+
+  const RentalDuration({this.unit, this.count});
+
+  factory RentalDuration.fromJson(Map<String, dynamic> json) {
+    return RentalDuration(
+      unit: json['unit'] as String?,
+      count: json['count'] as int?,
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        'unit': unit,
+        'count': count,
+      };
+
+  @override
+  List<Object?> get props => [unit, count];
+}
