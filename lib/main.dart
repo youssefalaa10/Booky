74a46393e68f1cb243e1d6827e507bbc1bd31064
youssefalaa10 <youssefalaacj@gmail.com>
2023-05-04 07:42:44 +0300

    setupServiceLocator using getIt

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:booky/Features/home/data/repos/home_repo_impl.dart';
+import 'package:booky/Features/home/presentation/view_models/books_home_cubit/books_home_cubit.dart';
+import 'package:booky/Features/home/presentation/view_models/newset_books_cubit/newset_books_cubit.dart';
 import 'package:booky/constants.dart';
 import 'package:booky/core/utils/app_router.dart';
+import 'package:booky/core/utils/services_locator/services_locator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  setupServiceLocator();
   runApp(const BookyApp());
 }
 
@@ -12,12 +18,27 @@ class BookyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => BooksHomeCubit(
+            getIt.get<HomeRepoImpl>(),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => NewsetBooksCubit(
+            getIt.get<HomeRepoImpl>(),
+          ),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme:
+              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        ),
       ),
     );
   }
